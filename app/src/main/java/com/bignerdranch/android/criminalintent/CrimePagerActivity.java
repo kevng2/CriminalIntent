0d524c3b207aa1f64f7225b19fdbe7734ea99c41
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-03-24 18:56:17 -0400

    beginning to add pagers

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,6 +1,29 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
 
 public class CrimePagerActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+    }
 }
